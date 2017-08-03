8227501eeffe1abd9903d7f9623098a037e799a7
Andrey Luiz <andrey.luiz@datarisk.io>
2017-08-03 01:22:59 -0300

    Add config files.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname),
+    libraryTarget: 'commonjs'
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+};
